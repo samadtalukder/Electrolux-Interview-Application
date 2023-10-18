c882f97743d550e396a95c7ac2e5e40f4fca8a78
Samad Talukder <samadtalukdar6@gmail.com>
2023-10-19 01:06:47 +0800

    implement parallel two tasks

---
+++ app/src/main/java/com/example/interviewapplication/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.interviewapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.interviewapplication.ui.theme.InterviewApplicationTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContent {
+
+            InterviewApplicationTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //Question3: Call taskOne and taskTwo methods below in a way that they run in parallel.
+
+                    var resultOne by remember { mutableStateOf(0) }
+                    var resultTwo by remember { mutableStateOf(0) }
+
+                    LaunchedEffect(true) {
+                        resultOne = async(Dispatchers.Default) { taskOne() }.await()
+                        resultTwo = async(Dispatchers.Default) { taskTwo() }.await()
+
+                    }
+
+                    ShowTaskData(resultOne, resultTwo)
+
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun ShowTaskData(resultOne: Int, resultTwo: Int) {
+        Column {
+            Text("TaskOne Result: $resultOne")
+            Text("TaskTwo Result: $resultTwo")
+        }
+    }
+
+
+    private suspend fun taskOne(): Int {
+        delay(2000)
+        return 10
+    }
+
+    private suspend fun taskTwo(): Int {
+        delay(2000)
+        return 10
+    }
+}
