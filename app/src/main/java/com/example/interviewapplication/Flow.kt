a0d1e9ccbb03cdfb26061461696c0992a625ec03
Samad Talukder <samadtalukdar6@gmail.com>
2023-10-19 01:08:34 +0800

    solved question1 & question2

---
+++ app/src/main/java/com/example/interviewapplication/Flow.kt
@@ -0,0 +1,57 @@
+package com.example.interviewapplication
+
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+interface CallbackApi {
+    fun fetchData(callback: (String?, Throwable?) -> Unit)
+}
+
+val apiWrapper = ApiWrapper(object : CallbackApi {
+    override fun fetchData(callback: (String?, Throwable?) -> Unit) {
+        // Simulate an asynchronous call
+        Thread.sleep(1000)
+        callback("Hello, world!", null)
+    }
+})
+
+class ApiWrapper(private val api: CallbackApi) {
+    //Question1: Call fetchData method in apiWrapper on the main thread.
+
+    private fun fetchData(): Flow<String> = flow {
+        val apiData = withContext(Dispatchers.IO) {
+            suspendCoroutine { ct ->
+                api.fetchData { s, error ->
+                    if (error != null) {
+                        ct.resumeWithException(error)
+                    } else if (s != null) {
+                        ct.resume(s)
+                    } else {
+                        ct.resumeWithException(NullPointerException("Data null"))
+                    }
+                }
+            }
+        }
+        emit(apiData)
+
+    }
+
+    fun test() {
+        //Question2: Call fetchData method above (which returns a flow) on a background thread.
+        runBlocking {
+            withContext(Dispatchers.IO) {
+                val mFlow = fetchData()
+                mFlow.collect { result ->
+                    Log.e("#", "Result: $result")
+                }
+            }
+        }
+    }
+}
